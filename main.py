63d470718e9030719e9544bb746a75e7a898e5de
KaylaCrush <kayla.e.rieck@gmail.com>
2024-02-28 14:51:58 -0800

    added chavez class to hold player attributes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ SCREEN_HEIGHT = 400
 BLACK = (0, 0, 0)
 SPRITE_SCALE = 1
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Carlos Game")
 
@@ -22,67 +22,49 @@ jumpRight = sprite_sheet.get_image(3,5, SPRITE_SCALE,BLACK)
 jumpLeft = sprite_sheet.get_image(1,5, SPRITE_SCALE,BLACK)
 jump = sprite_sheet.get_image(2,5, SPRITE_SCALE,BLACK)
 
-
 bg = pygame.image.load("images/school.jpg")
-
-pygame.font.init() # you have to call this at the start,
-                   # if you want to use this module.
-my_font = pygame.font.SysFont('Comic Sans MS', 30)
-text_surface = my_font.render('Kill', False, (0, 0, 0))
-
-
-
-x = 50
-y = SCREEN_HEIGHT - 70
-width = 64
-height = 64
-vel = 5
 
 clock = pygame.time.Clock()
 
-isJump = False
-jumpCount = 10
 
-left = False
-right = False
-walkCount = 0
+class chavez(object):
+    def __init__(self,x,y,width,height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 5
+        self.isJump = False
+        self.left = False
+        self.right = False
+        self.walkCount = 0
+        self.jumpCount = 10
+
+    def draw(self, win):
+        if self.walkCount + 1 >= 27:
+            self.walkCount = 0
+
+        if self.left:
+            win.blit(walkLeft[self.walkCount//3], (self.x,self.y))
+            self.walkCount += 1
+        elif self.right:
+            win.blit(walkRight[self.walkCount//3], (self.x,self.y))
+            self.walkCount +=1
+        else:
+            win.blit(stand, (self.x,self.y))
+
+
 
 def redrawGameWindow():
-    global walkCount
-
-    screen.blit(bg, (0,0))
-
-
-    pygame.draw.rect(screen, (255,0,0),(20,25,45,25))
-    screen.blit(text_surface, (20,20))
-
-    if walkCount + 1 >= 27:
-        walkCount = 0
-
-    if isJump:
-        if left:
-            screen.blit(jumpLeft,(x,y))
-        elif right:
-            screen.blit(jumpRight,(x,y))
-        else:
-            screen.blit(jump,(x,y))
-
-    elif left:
-        screen.blit(walkLeft[walkCount//3], (x,y))
-        walkCount += 1
-    elif right:
-        screen.blit(walkRight[walkCount//3], (x,y))
-        walkCount += 1
-    else:
-        screen.blit(stand, (x, y))
-        walkCount = 0
+    win.blit(bg, (0,0))
+    carlos.draw(win)
 
     pygame.display.update()
 
 
-
+#mainloop
+carlos = chavez(200, 410, 64,64)
 run = True
-
 while run:
     clock.tick(27)
 
@@ -92,34 +74,35 @@ while run:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT] and x > vel:
-        x -= vel
-        left = True
-        right = False
-
-    elif keys[pygame.K_RIGHT] and x < SCREEN_WIDTH - vel - width:
-        x += vel
-        left = False
-        right = True
-
+    if keys[pygame.K_LEFT] and carlos.x > carlos.vel:
+        carlos.x -= carlos.vel
+        carlos.left = True
+        carlos.right = False
+    elif keys[pygame.K_RIGHT] and carlos.x < 500 - carlos.width - carlos.vel:
+        carlos.x += carlos.vel
+        carlos.right = True
+        carlos.left = False
     else:
-        left = False
-        right = False
-        walkCount = 0
+        carlos.right = False
+        carlos.left = False
+        carlos.walkCount = 0
 
-    if not(isJump):
+    if not(carlos.isJump):
         if keys[pygame.K_SPACE]:
-            isJump = True
-            left = False
-            right = False
-            walkCount = 0
+            carlos.isJump = True
+            carlos.right = False
+            carlos.left = False
+            carlos.walkCount = 0
     else:
-        if jumpCount >= -10:
-            y -= (jumpCount * abs(jumpCount)) * 0.5
-            jumpCount -= 1
+        if carlos.jumpCount >= -10:
+            neg = 1
+            if carlos.jumpCount < 0:
+                neg = -1
+            carlos.y -= (carlos.jumpCount ** 2) * 0.5 * neg
+            carlos.jumpCount -= 1
         else:
-            jumpCount = 10
-            isJump = False
+            carlos.isJump = False
+            carlos.jumpCount = 10
 
     redrawGameWindow()
 
